9b400bb1ab7f5e4e06d2a0b1cad47c1315a6419b
Pete Kubiak <pete.kubiak@microbit.org>
2026-08-19 17:05:09 +0100

    Always send PIN_DATA_UPDATE reply if requested

--- source/bluetooth/MicroBitIOPinService.cpp
+++ source/bluetooth/MicroBitIOPinService.cpp
@@ -347,15 +347,12 @@ void MicroBitIOPinService::onDataRead(microbit_onDataRead_t *params)
 void MicroBitIOPinService::serialDataRequest(Event)
 {
     int pairs = updateBLEInputs(true);
-    if (pairs)
-    {
         uint8_t const dataLength = (sizeof(IOData) * pairs);
         uint8_t const payloadLength = dataLength + 1;
         uint8_t *const payloadBuffer = new uint8_t[payloadLength];
         payloadBuffer[0] = payloadLength;
         memcpy(&payloadBuffer[1], ioPinServiceIOData, dataLength);
         uartBle.sendMessage(PIN_DATA_UPDATE, payloadBuffer, payloadLength);
-    }
 }
 
 /**
